b096bb631b34e0909f7c0b09b6edcbcbaaf0addd
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-09-25 17:28:20 +0530

    Configuration pages: Filter page updated to mui v5
    Signed-off-by: Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>

--- ui/pages/configuration/filters.js
+++ ui/pages/configuration/filters.js
@@ -1,31 +1,33 @@
 import React, { useEffect } from 'react';
-import { NoSsr, withStyles } from '@material-ui/core';
+import { NoSsr } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import MesheryFilters from '../../components/Filters';
 import { updatepagepath } from '../../lib/store';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Head from 'next/head';
 import { getPath } from '../../lib/path';
+import { Box } from '@mui/material';
 
-const styles = {
-  paper: {
-    maxWidth: '90%',
-    margin: 'auto',
-    overflow: 'hidden',
-  },
-};
+const StyledBox = styled(Box)(() => ({
+  maxWidth: '90%',
+  margin: 'auto',
+  overflow: 'hidden',
+}));
 
 function NewFilters(props) {
   useEffect(() => {
     props.updatepagepath({ path: getPath() });
-  }, []);
+  }, [props]);
 
   return (
     <NoSsr>
       <Head>
         <title>Filters | Meshery</title>
       </Head>
-      <MesheryFilters />
+      <StyledBox>
+        <MesheryFilters />
+      </StyledBox>
     </NoSsr>
   );
 }
@@ -34,4 +36,6 @@ const mapDispatchToProps = (dispatch) => ({
   updatepagepath: bindActionCreators(updatepagepath, dispatch),
 });
 
-export default withStyles(styles)(connect(null, mapDispatchToProps)(NewFilters));
+const ConnectedNewFilters = connect(null, mapDispatchToProps)(NewFilters);
+
+export default ConnectedNewFilters;
